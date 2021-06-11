6b2abea1b774e3e5e7756e934e329a43b7e02d6d
VirgileLyon <vmv92i@hotmail.fr>
2021-06-11 10:47:35 +0200

    ini

---
+++ TrueTime/TrueTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueTimeApp.swift
+//  TrueTime
+//
+//  Created by Virgile Vercellis on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrueTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
