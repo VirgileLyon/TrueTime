c600ecf24cc839b93215bfc1c578290d228b81c1
VirgileLyon <vmv92i@hotmail.fr>
2021-06-14 11:59:05 +0200

    Add Button

--- TrueTime/MyTimeView.swift
+++ TrueTime/MyTimeView.swift
@@ -10,25 +10,56 @@ import SwiftUI
 struct MyTimeView: View {
     
     @State private var showDetails = false
+    @State private var showDetails2 = false
     
     var body: some View {
         NavigationView {
             VStack {
                 Text("Avez-vous pris du temps pour vous aujourd'hui?")
-                ZStack{
-                    Button("Show details") {
-                        showDetails.toggle()
-                    }
-                    if showDetails {
-                        Text("Nice! Keep going!")
-                            .font(.largeTitle)
+                VStack{
+                    HStack{
+                        Button("No time") {
+                            showDetails.toggle()
+                        }
+                        Button("5 Min") {
+                            showDetails.toggle()
+                        }
+                        Button("15 Min") {
+                            showDetails.toggle()
+                        }
+                        Button("30 Min") {
+                            showDetails.toggle()
+                        }
                     }
                 }
-            }
-            .navigationTitle("True Time")
+                if showDetails {
+                    Text("Nice! Keep going!")
+                        .font(.largeTitle)
+                }
+                VStack{
+                    HStack{
+                        Button("1 Hour") {
+                            showDetails2.toggle()
+                        }
+                        Button("2 Hour") {
+                            showDetails2.toggle()
+                        }
+                        Button("5 Hour") {
+                            showDetails2.toggle()
+                        }
+                        Button("All Day") {
+                            showDetails2.toggle()
+                        }
+                    }
+                }
+                if showDetails2 {
+                    Text("Insane!")
+                        .font(.largeTitle)
+                }
+            }.navigationTitle("True Time")
         }
-        
     }
+    
 }
 
 struct MyTimeView_Previews: PreviewProvider {
