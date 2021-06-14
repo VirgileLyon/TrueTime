b9a4d0ee623109b0a2be8286ebab344f8ead3f1b
VirgileLyon <vmv92i@hotmail.fr>
2021-06-14 11:09:31 +0200

    Préparation de la vue principale

--- TrueTime/MyTimeView.swift
+++ TrueTime/MyTimeView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct MyTimeView: View {
+    
+    @State private var showDetails = false
+    
     var body: some View {
-        Text("MyTimeView")
+        NavigationView {
+            VStack {
+                Text("Avez-vous pris du temps pour vous aujourd'hui?")
+                ZStack{
+                    Button("Show details") {
+                        showDetails.toggle()
+                    }
+                    if showDetails {
+                        Text("Nice! Keep going!")
+                            .font(.largeTitle)
+                    }
+                }
+            }
+            .navigationTitle("True Time")
+        }
+        
     }
 }
 
